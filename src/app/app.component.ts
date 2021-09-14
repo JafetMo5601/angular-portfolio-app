3d66c1a665903afa925ad443764ed647dabc626f
JafetMo5601 <jafet.moraugalde15@gmail.com>
2021-09-13 18:30:02 -0600

    Add the listeners of the application to make it responsive.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css'
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-portfolio-app';
+
+  ngOnInit() {
+
+    let hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+    let mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+    let menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+    let header = document.querySelector('.header.container');
+
+    hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('active');
+      mobile_menu.classList.toggle('active');
+    });
+
+    document.addEventListener('scroll', () => {
+      var scroll_position = window.scrollY;
+      if (scroll_position > 250) {
+        (header as HTMLElement).style.backgroundColor = '#29323c';
+      } else {
+        (header as HTMLElement).style.backgroundColor = 'transparent';
+      }
+    });
+
+    menu_item.forEach((item) => {
+      item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+      });
+    });
+  }
 }
